82d8c100b2a4129bd8b64708cde80dde4f3511a0
Rizwan Iliyas <rizwan@courseconcierge.com>
2025-04-05 13:58:04 +0530

    Plugin info update.

--- teammemberblock.php
+++ teammemberblock.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       Teammemberblock
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       A simple block plugin to create and add team members.
  * Version:           0.1.0
  * Requires at least: 6.7
  * Requires PHP:      7.4
